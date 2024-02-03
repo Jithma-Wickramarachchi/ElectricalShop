119f2bd7d7db9c591a33791d52746d8e5dd75b7d
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-02-03 14:09:50 +0530

    user dashboard form ui done

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,7 @@ S
 #.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
 ]
 -.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+r
+Bsrc/main/java/edu/icet/controller/UserDashboardFormController.java,5\2\5239239630f990a381a18d412f29698e4241ef66
+w
+Gsrc/main/java/edu/icet/controller/user/UserDashboardFormController.java,4\9\49f492c837bd44c89445b5ff147092c10ae4de7c
--- src/main/java/edu/icet/controller/LoginFormController.java
+++ src/main/java/edu/icet/controller/LoginFormController.java
@@ -1,14 +1,25 @@
 package edu.icet.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import lombok.Data;
 
 public class LoginFormController {
     public BorderPane loginPane;
 
     public void adminBtnOnAction(ActionEvent actionEvent) {
     }
-
     public void userBtnOnAction(ActionEvent actionEvent) {
+        try {
+            Stage stage = (Stage) loginPane.getScene().getWindow();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/user/UserDashboardForm.fxml"))));
+            stage.setTitle("User Dashboard");
+            stage.show();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
